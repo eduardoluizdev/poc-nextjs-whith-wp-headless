840851649e44594c562dc7adaab03732e0899ad5
Eduardo Luiz <hello@eduardoluiz.dev>
2022-04-20 10:47:16 -0300

    first commit

---
+++ src/shared/libs/withApollo.tsx
@@ -0,0 +1,21 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+export default withApollo(
+  ({ initialState }) => {
+    return new ApolloClient({
+      uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
+      cache: new InMemoryCache().restore(initialState || {})
+    });
+  },
+  {
+    render: ({ Page, props }) => {
+      return (
+        <ApolloProvider client={props.apollo}>
+          <Page {...props} />
+        </ApolloProvider>
+      );
+    }
+  }
+);
